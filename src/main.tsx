d6b572ea7ee32cf9dd7a90e1027ec8e59919766f
Matthew Reardon <patriotnewsactivism@proton.me>
2025-11-22 02:12:45 -0600

    Fix runtime env injection causing blank screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { injectRuntimeEnv } from "./config/runtime-env.ts";
+
+injectRuntimeEnv();
 
 createRoot(document.getElementById("root")!).render(<App />);
